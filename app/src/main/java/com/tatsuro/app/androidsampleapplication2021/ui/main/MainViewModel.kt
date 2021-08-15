63ffd43eddd715c2165f3a93e514a576d193946e
Tatsuro <tatsuro.secure.mail@protonmail.ch>
2021-08-15 15:11:06 +0900

    不要なimportの削除

--- app/src/main/java/com/tatsuro/app/androidsampleapplication2021/ui/main/MainViewModel.kt
+++ app/src/main/java/com/tatsuro/app/androidsampleapplication2021/ui/main/MainViewModel.kt
@@ -1,9 +1,7 @@
 package com.tatsuro.app.androidsampleapplication2021.ui.main
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
