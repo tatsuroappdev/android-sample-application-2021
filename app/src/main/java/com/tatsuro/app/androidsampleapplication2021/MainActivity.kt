e7d67ab3fd8b8735235483af136ff267b04207c8
Tatsuro <tatsuro.secure.mail@protonmail.ch>
2021-09-02 21:29:40 +0900

    Activityの修正
    レイアウトファイルの設定をコンストラクタで行う。

--- app/src/main/java/com/tatsuro/app/androidsampleapplication2021/MainActivity.kt
+++ app/src/main/java/com/tatsuro/app/androidsampleapplication2021/MainActivity.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.tatsuro.app.androidsampleapplication2021.ui.main.MainFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.main_activity) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
